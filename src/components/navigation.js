de4294a38a4cfa455ec7f9dd93514e49640210de
Divya Thapa <thapa.divya@gmail.com>
2021-01-29 14:36:35 +0000

    renamed gatsby-blog

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import styles from './navigation.module.css'
+import React from "react";
+import { Link } from "gatsby";
+import styles from "./navigation.module.css";
 
 export default () => (
   <nav role="navigation">
@@ -11,6 +11,15 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
+      <li className={styles.navigationItem}>
+        <Link to="/about/">About</Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/cities/">Cities</Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/home/">Homepage</Link>
+      </li>
     </ul>
   </nav>
-)
+);
---
+++ src/pages/cities.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cities = () => {
+  return (
+    <div>
+      <h1>London</h1>
+      <h1>Scotland</h1>
+      <h1>Manchester</h1>
+    </div>
+  );
+};
+
+export default Cities;
